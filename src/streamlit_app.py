9dfcfb36a5fbd028a0fd6a885ce3dcbfc11880ca
MathFinance01 <mathfinance01@gmail.com>
2024-10-24 10:14:23 +0530

    changing the pages to products

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -5,5 +5,5 @@ import QuantLib as ql
 #
 # st.form("contract_inputs", clear_on_submit=False, enter_to_submit=True, border=True)
 
-pg = st.navigation([st.Page("pages/1_options.py", default=True), st.Page("pages/2_fixed_income.py", default=False)])
+pg = st.navigation([st.Page("products/1_options.py", default=True), st.Page("products/2_fixed_income.py", default=False)])
 pg.run()
